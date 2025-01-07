4e8a084614e01330adfdfae8ab8f7416aba09bf7
smoaking90 <cwsmoak90@gmail.com>
2025-01-07 20:59:47 +0100

    linking the pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts', views.posts, name='posts-page'),
-    path('posts/<slug>', views.post, name='full-post')  # posts/my-first-post
+    path('posts/<slug:slug>', views.post, name='full-post')  # posts/my-first-post
 ]
